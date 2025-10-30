dc0ab22f349a2ea8aff056c57c8e470e9140ba9d
José Manuel Valderrama Sánchez <jmvs0008@red.ujaen.es>
2025-10-30 19:17:54 +0100

    bert entrenado con castle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datasets.load import DatasetDict
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
 from datasets import load_dataset
@@ -8,26 +9,43 @@ from huggingface_hub import login
 #login()
 
 # bert tiny necesita padding
-MODELS = ["microsoft/deberta-base-mnli", "google-bert/bert-base-cased", "prajjwal1/bert-tiny"]
-M = 1
-DATASETS = ["SetFit/emotion", 'yelp_review_full']
-D = 1
+"""
+"microsoft/deberta-base-mnli"
+"google-bert/bert-base-cased"
+"prajjwal1/bert-tiny"
+"""
+MODEL = "google-bert/bert-base-cased"
+"""
+"SetFit/emotion"
+'yelp_review_full'
+"""
+DATASET = "yelp_review_full"
 
 access_token = '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'
 
-# cargo un dataset y lo hago mas pequeño para poder entrenarlo rapido
-dataset = load_dataset(DATASETS[D])
-dataset["train"] = dataset["train"].shuffle(seed=42).select(range(6500))
-dataset["test"] = dataset["test"].shuffle(seed=42).select(range(1000))
 
-tokenizer = AutoTokenizer.from_pretrained(MODELS[M], token=access_token)
-print(dataset)
+tokenizer = AutoTokenizer.from_pretrained(MODEL, token=access_token)
 
-model = AutoModelForSequenceClassification.from_pretrained(MODELS[M], num_labels=5)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL, num_labels=2) # 2 etiquetas: vulnerable o no vulnerable
 
 def tokenize(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+    tk = tokenizer(
+        examples['code'],
+        padding="max_length",
+        truncation=True,
+        #max_length=512  # Add max_length to prevent excessive padding
+    )
+    # Convertimos True y False a 1 y 0
+    tk['labels'] = [int(v) for v in examples['vulnerable']]
+    return tk
 
+dataset = load_dataset('json', data_files='datasets/CASTLE-C250.json', field='tests')
+# dividir en train (80%) y test (20%)
+train_test = dataset['train'].train_test_split(test_size=0.2, seed=42)
+dataset = DatasetDict({
+    'train': train_test['train'],
+    'test': train_test['test']
+})
 dataset = dataset.map(tokenize, batched=True)
 
 metric = evaluate.load("accuracy")
@@ -39,18 +57,24 @@ def compute_metrics(eval_pred):
     return metric.compute(predictions=predictions, references=labels)
 
 training_args = TrainingArguments(
-    output_dir="yelp_review_classifier",
-    eval_strategy="epoch",
+    output_dir="training/castle",
+    eval_strategy='epoch',  # Enable evaluation
     per_device_train_batch_size=10,
     per_device_eval_batch_size=10,
-    num_train_epochs=1,
+    num_train_epochs=3,
+    #logging_dir='./logs',
+    #logging_steps=100,
+    #save_strategy="epoch",
+    #load_best_model_at_end=True,
+    #metric_for_best_model="accuracy"
 )
+
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    compute_metrics=compute_metrics,
+    train_dataset=dataset['train'],
+    eval_dataset=dataset['test'],  # Uncomment this
+    compute_metrics=compute_metrics,  # Uncomment this
 )
 trainer.train()
 """
