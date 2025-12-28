fdcd0fc6bbafca49573af809c2ecd93e23df2bf5
Jose Manuel Valderrama Sánchez <jmvs0008@red.ujaen.es>
2025-12-28 11:02:42 +0100

    limpieza en la salida

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-def check_dirs(model_path, tokenizer_path):
+def check_dirs(model_path, tokenizer_path, code_path):
     import os
 
     if not os.path.exists(model_path):
-        raise FileNotFoundError(f"{model_path} no existe.")
+        raise FileNotFoundError(f"La ruta al modelo '{model_path}' no existe.")
     if not os.path.exists(tokenizer_path):
-        raise FileNotFoundError(f"{tokenizer_path} no existe.")
+        raise FileNotFoundError(f"La ruta al tokenizador '{tokenizer_path}' no existe.")
+    if not os.path.exists(code_path):
+        raise FileNotFoundError(f"La ruta {code_path} no existe. Actualiza el archivo de configuración con una ruta válida al código a analizar.")
 
 def get_code(file_path: str) -> str:
     """
@@ -48,7 +50,7 @@ if __name__ == "__main__":
         logger.log("test.py", f"Model path: {model}")
         logger.log("test.py", f"Tokenizer path: {tokenizer}")
         
-        check_dirs(model, tokenizer)
+        check_dirs(model, tokenizer, settings["code_file"])
         logger.log_step("check_dirs", "Directories verified", "COMPLETED")
 
         classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
@@ -60,9 +62,10 @@ if __name__ == "__main__":
         result = classifier(code)
         logger.log("classifier", f"Classification result: {result}")
         
-        print(result)
         
         logger.finalize("test.py", "SUCCESS")
+        output = "El codigo es vulnerable" if result[0]['label'] == 'VULNERABLE' else "El código es seguro"
+        print(output + " con una probabilidad del {:.2f}%".format(result[0]['score'] * 100))
         
     except Exception as e:
         logger.log_error("test.py", e)
