a235af68c09351253da1401c836c278048ac9327
Jose Manuel Valderrama Sanchez <jmvs0008@red.ujaen.es>
2026-02-27 11:03:38 +0100

    pruebas post-reunion

--- finetune.py
+++ finetune.py
@@ -1,12 +1,24 @@
+from transformers import TrainerCallback
+
+class EarlyStoppingCallback(TrainerCallback):
+    def __init__(self, num_steps=10):
+        self.num_steps = num_steps
+
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step >= self.num_steps:
+            return {"should_training_stop": True}
+        else:
+            return {}
+
 def get_model(settings):
     from transformers import AutoModelForSequenceClassification
 
     id2label = {0: "SAFE", 1: "VULNERABLE"}
     label2id = {"SAFE": 0, "VULNERABLE": 1}
 
-    import os
-    if not os.path.exists(settings["model_path"]):
-        raise ValueError(f"El path del modelo no existe: {settings['model_path']}. Asegúrate de haber descargado el modelo correctamente según las instrucciones en README.")
+    #import os
+    #if not os.path.exists(settings["model_path"]):
+    #    raise ValueError(f"El path del modelo no existe: {settings['model_path']}. Asegúrate de haber descargado el modelo correctamente según las instrucciones en README.")
 
     return AutoModelForSequenceClassification.from_pretrained(
         settings["model_path"], 
@@ -51,13 +63,13 @@ def train(model, dataset):
         metric_for_best_model="accuracy",
         per_device_train_batch_size=settings["batch_size"],
         per_device_eval_batch_size=settings["batch_size"],
-        gradient_accumulation_steps=settings.get("gradient_accumulation_steps", 1),
+        #gradient_accumulation_steps=settings["gradient_accumulation_steps"],
         num_train_epochs=settings["num_epochs"],
+        greater_is_better=True,
         # logs
         logging_dir=TRAINING_LOG_PATH,
         logging_steps=10,
         report_to="tensorboard",
-        #load_best_model_at_end=True,
     )
     trainer = Trainer(
         model=model,
@@ -65,6 +77,9 @@ def train(model, dataset):
         train_dataset=dataset['train'],
         eval_dataset=dataset['test'],
         compute_metrics=compute_metrics,
+        processing_class=dataset.tokenizer,
+        #data_collator=data_collator,
+        callbacks=[EarlyStoppingCallback()],
     )
 
     trainer.train()
